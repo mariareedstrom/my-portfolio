ff957ae6be923903ef51414cf89595dc7314984a
Maria Reedstrom <maria.reedstrom@gmail.com>
2023-04-26 09:38:58 -0500

    bug fix

--- app/projects/kittycatgenerator/page.tsx
+++ app/projects/kittycatgenerator/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import kittyCatImg from "/public/images/kittycatgenerator.png";
+import kittyCatImg from "/public/images/kittycatgenerator.jpg";
 import Link from "next/link";
 
 export default function Project() {
